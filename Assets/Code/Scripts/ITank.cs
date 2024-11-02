63fc4d622c0b67ec4d8bbc918fd3b8f889bbb2d9
i-ka <i-ka2010@mail.ru>
2024-11-03 00:42:15 +0500

    Use team enum instead on side

--- Assets/Code/Scripts/ITank.cs
+++ Assets/Code/Scripts/ITank.cs
@@ -1,10 +1,11 @@
 using System;
+using Code.Scripts.AI.Data;
 
 namespace SibGameJam
 {
     public interface ITank
     {
         event Action<ITank> OnDestroyed;
-        Side Side { get; }
+        Team Team { get; }
     }
 }
