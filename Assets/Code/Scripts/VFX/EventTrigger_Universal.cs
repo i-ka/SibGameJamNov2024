90f02c5bfbec43a6091a06aa23f082377420343f
Anton Zarubin <anzzamaker@gmail.com>
2024-11-04 01:58:06 +1000

    Prepare screw for spawn and pick up

--- Assets/Code/Scripts/VFX/EventTrigger_Universal.cs
+++ Assets/Code/Scripts/VFX/EventTrigger_Universal.cs
@@ -6,9 +6,14 @@ public class EventTriggerUniversal : MonoBehaviour
 {
 
     [SerializeField] private VisualEffect visualEffect;
+    [SerializeField] private Collider targetCollider;
     public string Event = "Event Name";
     private void OnTriggerEnter(Collider other) 
     {
-        visualEffect.SendEvent(Event);
+        if (other == targetCollider) 
+        {
+            visualEffect.SendEvent(Event);
+        }
+        
     }
 }
