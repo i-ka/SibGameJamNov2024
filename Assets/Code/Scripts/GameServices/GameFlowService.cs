812a99dfce27162557b0031451a6050607107d72
Tair Vildanov <tairvildanov7@gmail.com>
2024-11-04 19:20:34 +0500

    Towers registry fix

--- Assets/Code/Scripts/GameServices/GameFlowService.cs
+++ Assets/Code/Scripts/GameServices/GameFlowService.cs
@@ -4,22 +4,22 @@ using VContainer.Unity;
 
 namespace Code.Scripts.GameServices
 {
-    public class GameFlowService : IInitializable
-    {
-        public event Action OnWin;
-        public event Action OnLose; 
-        
-        private readonly FactoryRegistry factoryRegistry;
-        
-        public GameFlowService(FactoryRegistry factoryRegistry)
-        {
-            this.factoryRegistry = factoryRegistry;
-        }
-        
-        public void Initialize()
-        {
-            factoryRegistry.GetFabric(Team.Red).HealthController.OnDestroyed += () =>  OnLose?.Invoke();
-            factoryRegistry.GetFabric(Team.Blue).HealthController.OnDestroyed += () => OnWin?.Invoke();
-        }
-    }
+	public class GameFlowService : IInitializable
+	{
+		public event Action OnWin;
+		public event Action OnLose;
+
+		private readonly TowerRegistry towerRegistry;
+
+		public GameFlowService(TowerRegistry towerRegistry)
+		{
+			this.towerRegistry = towerRegistry;
+		}
+
+		public void Initialize()
+		{
+			towerRegistry.GetTower(Team.Red).HealthController.OnDestroyed += () => OnLose?.Invoke();
+			towerRegistry.GetTower(Team.Blue).HealthController.OnDestroyed += () => OnWin?.Invoke();
+		}
+	}
 }
