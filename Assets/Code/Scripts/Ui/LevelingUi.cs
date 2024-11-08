82cc537331512cd58296d710a2da382fee1a1865
i-ka <i-ka2010@mail.ru>
2024-11-08 22:01:54 +0500

    Replace sliders with value bars

--- Assets/Code/Scripts/Ui/LevelingUi.cs
+++ Assets/Code/Scripts/Ui/LevelingUi.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Code.Scripts.Ui;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,13 +13,13 @@ namespace SibGameJam.Ui
         public TextMeshProUGUI LevelText { get; private set; }
         
         [field:SerializeField]
-        public Slider CurrentLevelProgres { get; private set; }
+        public ValueBar CurrentLevelProgres { get; private set; }
 
         [field: SerializeField]
         public TextMeshProUGUI FactoryLevelText { get; private set; }
 
         [field:SerializeField]
-        public Slider FactoryCurrentLevelProgress { get; private set; }
+        public ValueBar FactoryCurrentLevelProgress { get; private set; }
     }
 }
 
